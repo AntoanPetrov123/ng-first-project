06d1f07f2e463aa138e3110ab558b8c9bd981b7c
Antoan Petrov <antoan999_kn@abv.bg>
2022-04-08 16:14:31 +0300

    Bugs

--- src/app/feature/cars/catalog-page/catalog-page.component.ts
+++ src/app/feature/cars/catalog-page/catalog-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/user.service';
 
 @Component({
   selector: 'app-catalog-page',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogPageComponent implements OnInit {
 
-  constructor() { }
+  
 
+  constructor() {
+  }
   ngOnInit(): void {
   }
 
